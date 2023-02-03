1f78dda69d5e8149c540890fb41de07eb3d8e006
Mohit Kurwade <mohitkurwade@gmail.com>
2023-02-04 04:37:24 +0530

    hibernate CRUD operation

---
+++ src/main/java/com/jspiders/hibernate/dao/StudentDAOInsert.java
@@ -0,0 +1,35 @@
+package com.jspiders.hibernate.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import com.jspiders.hibernate.dto.StudentDTO;
+
+public class StudentDAOInsert {
+	
+	public static void main(String[] args) {
+		
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("student");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		entityTransaction.begin();
+		
+		StudentDTO student = new StudentDTO();
+		student.setId(1);
+		student.setName("popatlal");
+		student.setEmail("popatlal@matrimony.com");
+		student.setContact(77347476);
+		student.setCity("Gokuldham");
+		
+		entityManager.persist(student);
+		entityTransaction.commit();
+		entityManagerFactory.close();
+		entityManager.close();
+		
+
+		
+	}
+
+}
